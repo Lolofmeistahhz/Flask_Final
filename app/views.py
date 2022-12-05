a83982b1193f97519d2a77a58403847ce7f9d450
Lolofmeistahhz <99910272+Lolofmeistahhz@users.noreply.github.com>
2022-12-05 21:33:13 +0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -70,15 +70,9 @@ def do_order():
 
 
 @app.route('/update_server', methods=['POST'])
-
-
-def webhook():
-    if request.method == 'POST':
-        repo = git.Repo('path/to/git_repo')
-        origin = repo.remotes.origin
-
-        origin.pull()
-
-        return 'Updated PythonAnywhere successfully', 200
-    else:
-        return 'Wrong event type', 400
+def update_server():
+    repo = git.Repo('./Flask_Final')
+    origin = repo.remotes.origin
+    repo.create_head("main",origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
+    origin.pull()
+    return 'Updated PythonAnywhere successfully', 200
